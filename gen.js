37ea3b1ce58781aba35a47c9295ae1fd41b58141
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2021-10-13 17:10:54 +0630

    import external quote

--- gen.js
+++ gen.js
@@ -1,7 +1,12 @@
 import {readFileSync,writeFileSync} from 'fs'
 import {alphabetically0} from 'pitaka/utils'
+import pinpoint from '../pitaka/cli/pinpoint.js'
 //clone from https://github.com/g0v/moedict-data
 import hotfixes from './hotfix.js'
+import lit_12_quote from './lit_12_quote.js'
+import lit_6_quote from './lit_6_quote.js'
+const PinPoints={openlit:[lit_12_quote, lit_6_quote]} ;
+
 const pua={'90ba':'𥳑'}
 const tidy=str=>str
 .replace(/&/g,'＆').replace(/</g,'＜').replace(/>/g,'＞')
@@ -11,7 +16,6 @@ const tidy=str=>str
 
 const content=tidy(readFileSync('./dict-revised.json','utf8'));
 const gycd=JSON.parse(content)
-import {normalizeText} from './moedict-utils.js';
 
 // const gycd=JSON.parse(readFileSync('./moedict-sample.json','utf8'))
 //缺字一律改為● {[9951]}
@@ -58,19 +62,52 @@ const extractQuote=str=>{
     return {def,quotes}
 }
 const entries=[];
+const markupDef=def=>{
+   def=def.replace(/(也作|參見)「([^」]+)」(、「[^」]+」)*/g,(m,m1,m2,m3)=>{
+        let s=m1+'^ref['+m2+']';
+        if (m3) {
+            s+=m3.replace(/「([^」]+)」/g,(m,m4)=>{
+                return '^ref['+m4+']';
+            })
+        }
+        return s;
+   })
+   
 
+   return def;
+}
 
+const getPinPoints=title=>{
+    const out=[];
+    for (let ptk in PinPoints) {
+        PinPoints[ptk].forEach(pp=>{
+            if (pp[title]) out.push(... pp[title].map(i=>i.replace('^','^/'+ptk+'/'))) 
+        });
+    }
+    return out;
+}
 const parseField=fields=>{
-    const title=fields.title.replace(/\{\[....\]\}/,'●');
+    let title=fields.title.replace(/\{\[....\]\}/,'●');
     //0x2780
+    let en='';
     const words_defs=[];
     if (fields.heteronyms.length>maxproncount) maxproncount=fields.heteronyms.length;
     for (let i=0;i<fields.heteronyms.length;i++) {
         const defs=[];
+        
         const entry=fields.heteronyms[i];
         if (entry.definitions.length>maxdefcount) maxdefcount=entry.definitions.length;
 
-        if (title[0]=='{') continue;
+        if (title[0]=='{' ||title==' ') continue;
+        let at=title.indexOf('（');
+        const at2=title.indexOf('(');
+        if (at>-1||at2>-1) {
+        	if (at2>at) at=at2;
+        	en=title.substr(at+1);
+        	if (en[en.length-1]=='）'||en[en.length-1]==')' ) en=en.substr(0,en.length-1);
+        	title=title.substr(0,at);
+    	}
+        
         let py='';
         if (fields.heteronyms.length==1) {
             //py=(entry.pinyin&&!entry.pinyin.indexOf(' ')==-1?(' '+entry.pinyin):'');
@@ -82,14 +119,17 @@ const parseField=fields=>{
         for (let j=0;j<entry.definitions.length;j++) {
             const D=entry.definitions[j];
             
-            const {def,quotes}=extractQuote(D.def);
+            let {def,quotes}=extractQuote(D.def);
+            def=markupDef(def);
             const examples=[];
             if(D.example)for (let k=0;k<D.example.length;k++) examples.push(D.example[k]);
-            if(D.quote)for (let k=0;k<D.quote.length;k++) quotes.push(D.quote[k]);
+            if(D.quote)for (let k=0;k<D.quote.length;k++) {
+                quotes.push(D.quote[k]);
+            }
 
-            defs.push('^d '+(D.type?('['+D.type+']'):'')+def
+            defs.push('^d'+(D.type?('[o='+D.type+']'):' ')+def
             +(examples.length?'\n':'')
-            +examples.map(m=>'^ex '+m)
+            +examples.map(m=>'^eg '+m)
             +(quotes.length?'\n':'')
             +quotes.map(m=>'^q '+m)
             .join('\n'));
@@ -97,39 +137,74 @@ const parseField=fields=>{
             if (!types[D.type]) types[D.type]=0;
             types[D.type]++;
         }   
-
         words_defs.push([py,defs]);
     }
 
-    if (title[0]!=='●')  entries.push( [normalizeText(title), title, words_defs]);
+    if (title[0]!=='●' && title[0]!==' ')  entries.push( [title, en,words_defs]);
 }
 console.log(`parsing ${gycd.length} entry`);
 for (let i in  gycd) parseField(gycd[i]);
 
-
-const out=[];
 /*
 make sure 不丹 come before 不丹王國
 <H>不丹（Bhutan）
 <H>不丹王國（Kingdom of Bhutan）
 */
 entries.sort(alphabetically0);
-
-entries.forEach(([normalized,title,words_defs])=>{
+const out=[];
+entries.forEach(([title,en,words_defs])=>{
     let prevc='';
+    const entry=[];
     words_defs.forEach( ([py,defs])=>{
-        if (title!==prevc) out.push('^c '+title);  
-        py&&out.push('^y '+py);  
+        if (title!==prevc) {
+            out.push(...entry);
+            entry.length=0;
+        	entry.push('^e '+title);
+        	en&&entry.push('^en '+en);
+        }
+        py&&entry.push('^y '+py);
         // 詞條相同者，以音區分，定位簡單，缺點是 不同音的字的詞被拆開
         // 如「行規」háng、 「行動」xíng本應歸在不同的「行」詞群，但目前按逐字排序，所以被合併起來。
-        
-        let content=out.join('\n');
+
         if (hotfixes[title]) {
+            let content=defs.join('\n');
             hotfixes[title].forEach(rep=>content=content.replace(rep[0],rep[1]));
+            defs=content.split('\n')
+        } else {
+            defs=defs.join('\n').split('\n');
         }
-        out.push(content);
+
+        const pp=getPinPoints(title);
+        if (pp.length) {        
+            for (let i=0;i<pp.length;i++) {
+                if (!pp[i]) continue;
+                const [y,target] = pp[i].split('^');
+                
+                const line = parseInt(y)-entry.length ;
+                if (!defs[line]) continue;
+
+                
+                if (defs[line].indexOf('^q')==-1) {
+                    throw 'pinpoint failed '+line+' '+title+' '+target;
+                }
+                
+                const L=defs[line].substr(3);
+                const start=L.indexOf('「');
+                const end=L.lastIndexOf('」');
+                if (start==-1||end==-1||start>end) {
+                    throw 'quote text not found '+defs[line];
+                }                
+                defs[ line ] = L.substr(0,start) + '^t[@'+target+' '+L.substring(start,end+1)+']'+L.substr(end+1);
+                pp[i]=null;
+            }
+        }
+
+
+        entry.push(...defs);
         prevc=title;
     })
+    out.push(...entry);
+    
 })
 
 // console.log(`max pronounce ${maxproncount} max def count ${maxdefcount}`)
